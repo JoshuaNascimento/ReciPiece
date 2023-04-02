af30f7e258daa9e090c464f64fac2bafdb854543
Joshua Nascimento <joshua.l.nascimento@gmail.com>
2023-04-02 18:50:23 -0400

    Moved component functionality out of pages

--- src/client/src/Pages/Recipes.js
+++ src/client/src/Pages/Recipes.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
-import { Card, CardHeader, CardMedia, Box, CardContent, Typography, ListItem } from '@mui/material';
+import { Grid } from '@mui/material';
 import IngredientSearch from '../Components/IngredientSearch.js';
+import RecipeCard from '../Components/RecipeCard.js';
 
 const sxStyle = {
   display: 'flex',
@@ -9,42 +10,31 @@ const sxStyle = {
   justifyContent: 'center',
   height: '100vh',
   background: '#FEDBD0',
+
 }
 
 export default function Recipes() {
 
   const location = useLocation();
 
-  const data = Array.from(location.state.recipes);
-
   return (
 
     // Main recipe container
-    <Box display="flex" sx={sxStyle}>
+    <Grid container sx={sxStyle}>
       {/* Use IngredientSearch with the ingredients used to generate the current recipes data */}
-      <IngredientSearch ingredients={location.state.ingredients} />
+      <Grid item xs={2}>
+        <IngredientSearch sx={ {padding:"5"} } ingredients={location.state.ingredients} />
+      </Grid>
+
+      <Grid item xs={1}>
+        <br></br>
+      </Grid>
+      
       {/* Recipe card Box */}
-      <Box sx={{ display: 'flex', justifyContent: 'center', mt: 20, gap: 5 }}>
-        {data.slice(0, 3).map(recipe => (       // Grab recipe data from api call and map only the beginning 3 values from it
-          <Card sx={{ padding: 0 }}>            {/* Use a Card to hold data for each individual recipe */}
-            <CardHeader title={recipe.title} /> {/* CardHeader is the "title" used within the Card */}
-            <CardContent>                       {/* Stores the main "body" of the given Card */}
-              <CardMedia component='img' image={recipe.image} height='140' /> {/* Stores the image associated with each recipe */}
-              <Typography variant="h5" align="center">  {/* Typography is a customizable text tag being utalized as a subheader within a Card */}
-                Missing Ingredients:
-              </Typography>
-              
-              { // Map out all the missing ingredients that will be required prior to cooking the recipe
-                Array.from(recipe.missedIngredients).map(miss => (
-                  <ListItem dense="true">       {/* Display each missing ingredient */}
-                    {miss.name}
-                  </ListItem>
-                ))
-              }
-            </CardContent>
-          </Card>
-        ))}
-      </Box>
-    </Box>
+      <Grid item xs={8}>
+        <RecipeCard  data={location.state.recipes}/>
+      </Grid>
+      
+    </Grid>
   )
 }
